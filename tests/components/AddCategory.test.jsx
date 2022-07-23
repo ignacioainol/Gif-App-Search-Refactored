577d1d06032abc2e07ae44a6c6e0e221ec086755
Ignacio Ainol <iggnaxios@gmail.com>
2022-07-23 17:17:54 -0400

    debe de llamar owCategory

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -2,11 +2,27 @@ import { fireEvent, render, screen } from "@testing-library/react"
 import { AddCategory } from "../../src/components/AddCategory"
 
 describe('Test in AddCategory', () => {
+
   test('Debe de cambiar el valor de la caja de texto', () => {
     render(<AddCategory onNewCategory={() => { }} />);
     const input = screen.getByRole('textbox');
 
     fireEvent.input(input, { target: { value: 'Saitama' } })
     expect(input.value).toBe('Saitama');
+  })
+
+  test('Debe de llamar onNewCategory si el input tiene un valor', () => {
+
+    const inputValue = 'Saitama';
+
+    render(<AddCategory onNewCategory={() => { }} />);
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input, { target: { value: inputValue } })
+    fireEvent.submit(form);
+
+    expect(input.value).toBe('');
+
   })
 })
